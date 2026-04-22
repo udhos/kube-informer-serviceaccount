394086bede82dea527120341cc0bcd6521b769dc
udhos <udhos>
2026-04-22 01:15:49 -0300

    Upgrade dependencies.

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ go install golang.org/x/tools/cmd/deadcode@latest
 go install github.com/mgechev/revive@latest
 go install honnef.co/go/tools/cmd/staticcheck@latest
 go install golang.org/x/tools/go/analysis/passes/modernize/cmd/modernize@latest
+go install github.com/gordonklaus/ineffassign@latest
+go install github.com/client9/misspell/cmd/misspell@latest
+go install github.com/fzipp/gocyclo/cmd/gocyclo@latest
 
 gofmt -s -w .
 
@@ -15,6 +18,10 @@ staticcheck ./...
 modernize -fix ./...
 
 gocyclo -over 15 .
+
+ineffassign ./...
+
+misspell .
 
 go mod tidy
 
